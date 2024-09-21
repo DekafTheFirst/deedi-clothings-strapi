6bdd63ecee7f570a47d62c92922774db2484bb8b
Destiny Kefas <dekeji1@gmail.com>
2024-09-21 21:12:02 +0100

    url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_URL', 'https://deedis.com/server'),
   app: {
     keys: env.array('APP_KEYS'),
   },
