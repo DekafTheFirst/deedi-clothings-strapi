f21a61e272d18e578082beb103c7cf539d0c16f1
Destiny Kefas <dekeji1@gmail.com>
2024-09-23 21:37:14 +0100

    not required

--- src/api/cart-item/content-types/cart-item/lifecycles.js
+++ src/api/cart-item/content-types/cart-item/lifecycles.js
@@ -1,12 +1,1 @@
-module.exports = {
-    async beforeCreate(event) {
-        const { data } = event.params;
-        // console.log('Before Create Data:', data);
 
-        if (!data.product) {
-            throw new Error('Product is required');
-        }
-
-        // Optionally log for debugging
-    },
-};
--- src/api/stock/content-types/stock/lifecycles.js
+++ src/api/stock/content-types/stock/lifecycles.js
@@ -5,91 +5,92 @@ const { ApplicationError } = require('@strapi/utils').errors;
 
 
 module.exports = {
-    async beforeCreate(event) {
-        console.log(event.params)
-        const { data } = event.params;
-        const { product, size } = data;
+    // async beforeCreate(event) {
+    //     console.log(event.params)
+    //     const { data } = event.params;
+    //     const { product, size } = data;
 
-        // console.log('product', product);
-        // console.log('size', size);
-
-
-        if (product?.connect?.length <= 0) {
-            throw new ApplicationError('Product is required.', { foo: 'bar' });
-        }
-
-        if (size?.connect?.length <= 0) {
-            throw new ApplicationError('Size is required.');
-        }
+    //     // console.log('product', product);
+    //     // console.log('size', size);
 
 
+    //     if (product?.connect?.length <= 0) {
+    //         throw new ApplicationError('Product is required.', { foo: 'bar' });
+    //     }
 
-        const existingStock = await strapi.db.query('api::stock.stock').findOne({
-            where: {
-                product: {
-                    id: product.connect[0].id,
-                },
-                size: {
-                    id: size.connect[0].id,
-                },
-            },
-        });
-
-        if (existingStock) {
-            throw new Error('Stock entry for this product-size combination already exists.');
-        }
-    },
-    async beforeUpdate(event) {
-        const { data } = event.params;
-        const { product, size } = data
-
-        // console.log('product', product);
-        // console.log('size', size);
-        // const query = {};
-        // if () {
-        //     query['product'] = product?.connect?.[0]?.id || product?.id;
-        // }
-        // if () {
-        //     query['size'] = size?.connect?.[0]?.id || size?.id;
-        // }
+    //     if (size?.connect?.length <= 0) {
+    //         throw new ApplicationError('Size is required.');
+    //     }
 
 
 
-        // Ensure query is not empty
-        // if (Object.keys(query).length === 0) {
-        //     return; // No relevant fields to check
-        // }
+    //     const existingStock = await strapi.db.query('api::stock.stock').findOne({
+    //         where: {
+    //             product: {
+    //                 id: product.connect[0].id,
+    //             },
+    //             size: {
+    //                 id: size.connect[0].id,
+    //             },
+    //         },
+    //     });
 
-        // Check if product or size was disconnected and throw error
-        if (product?.disconnect?.length > 0) {
-            throw new Error('Product is required');
-        }
+    //     if (existingStock) {
+    //         throw new Error('Stock entry for this product-size combination already exists.');
+    //     }
+    // },
 
-        if (size?.disconnect?.length > 0) {
-            throw new Error('Size is required');
-        }
+    // async beforeUpdate(event) {
+    //     const { data } = event.params;
+    //     const { product, size } = data
+
+    //     // console.log('product', product);
+    //     // console.log('size', size);
+    //     // const query = {};
+    //     // if () {
+    //     //     query['product'] = product?.connect?.[0]?.id || product?.id;
+    //     // }
+    //     // if () {
+    //     //     query['size'] = size?.connect?.[0]?.id || size?.id;
+    //     // }
 
 
-        let existingStock
 
-        if (size?.connect?.length > 0 && product?.connect?.length > 0) {
-            existingStock = await strapi.db.query('api::stock.stock').findOne({
-                where: {
-                    product: {
-                        id: product?.connect?.[0]?.id,
-                    },
-                    size: {
-                        id: size?.connect[0]?.id,
-                    },
-                    id: {
-                        $ne: event?.params?.where?.id,  // Ensure it's not checking the current entry
-                    },
-                },
-            });
-        }
+    //     // Ensure query is not empty
+    //     // if (Object.keys(query).length === 0) {
+    //     //     return; // No relevant fields to check
+    //     // }
 
-        if (existingStock) {
-            throw new Error('Stock entry for this product-size combination already exists.');
-        }
-    },
+    //     // Check if product or size was disconnected and throw error
+    //     if (product?.disconnect?.length > 0) {
+    //         throw new Error('Product is required');
+    //     }
+
+    //     if (size?.disconnect?.length > 0) {
+    //         throw new Error('Size is required');
+    //     }
+
+
+    //     let existingStock
+
+    //     if (size?.connect?.length > 0 && product?.connect?.length > 0) {
+    //         existingStock = await strapi.db.query('api::stock.stock').findOne({
+    //             where: {
+    //                 product: {
+    //                     id: product?.connect?.[0]?.id,
+    //                 },
+    //                 size: {
+    //                     id: size?.connect[0]?.id,
+    //                 },
+    //                 id: {
+    //                     $ne: event?.params?.where?.id,  // Ensure it's not checking the current entry
+    //                 },
+    //             },
+    //         });
+    //     }
+
+    //     if (existingStock) {
+    //         throw new Error('Stock entry for this product-size combination already exists.');
+    //     }
+    // },
 };
